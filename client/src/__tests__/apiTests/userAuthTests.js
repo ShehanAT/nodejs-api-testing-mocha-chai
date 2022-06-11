ec17912f33ed5506367b11bd773945bc606e6e31
Sean Atukorala <shehanatuk@gmail.com>
2022-06-11 17:34:51 -0400

    added new server app: new_server; porting over server app code to new_server

--- client/src/__tests__/apiTests/userAuthTests.js
+++ client/src/__tests__/apiTests/userAuthTests.js
@@ -11,3 +11,38 @@ import userSeeder from '../server/seeders/userSeeder';
 
 const server = supertest.agent(app);
 const token = process.env.testToken;
+
+before((done) => {
+    models.sequelize.sync({ force: true }).then(() => {
+      done(null);
+    }).catch((errors) => {
+      done(errors);
+    });
+});
+
+
+describe('User Api: ', () => { 
+
+    it('should check that username exceeds 4 characters', (done) => {
+        server
+          .post('/api/v1/users/signup')
+          .set('Connection', 'keep alive')
+          .set('Content-Type', 'application/json')
+          .type('form')
+          .send(usernameMin5)
+          .expect(400)
+          .end((err, res) => {
+            res.status.should.equal(400);
+            res
+              .body[0]
+              .error
+              .should.equal('Please provide a username with atleast 4 characters.');
+            done();
+          });
+      });
+
+
+
+
+
+});
